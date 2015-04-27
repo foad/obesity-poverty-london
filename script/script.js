aa617356a242f398bd75fa4264bbf6eabedf4129
Dan Foad <danielfoad@gmail.com>
2015-04-27 23:38:47 +0100

    Please work

--- script/script.js
+++ script/script.js
@@ -96,7 +96,7 @@ function readSource(url, pipeDelimited) {
                     data.push(JSON.parse(raw[i]));
                 }
             } else {
-                data = raw;
+                data = eval(raw);
             }
         }
     };
