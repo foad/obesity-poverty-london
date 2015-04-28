98c74b4d78e639a4ef1ffc0f8eefd2b13373d77b
Dan Foad <danielfoad@gmail.com>
2015-04-28 20:54:01 +0100

    More work on making outlines work

--- script/script.js
+++ script/script.js
@@ -8,6 +8,7 @@ var mapOptions = {
     // Styling to remove POI labels and mute road colours
     styles: [{"stylers":[{"saturation":0}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":200},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"simplified"},{"saturation":45}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"simplified"},{"saturation":-45}]},{"featureType":"water","elementType":"geometry","stylers":[{"visibility":"simplified"},{"saturation":45}]},{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"simplified"},{"saturation":45}]},{"featureType":"transit","elementType":"labels","stylers":[{"visibility":"simplified"},{"saturation":45}]},{elementType: "labels",stylers: [{ visibility: "off" }]}]
 };
+var outlineData; // Data from read in outline points
 
 // Sources
 var obesity_source = "sources/obesity.txt"; // Data from http://data.london.gov.uk/dataset/obesity-adults
@@ -19,7 +20,7 @@ function initialise() {
     // Initialise map object with relevant options
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    var outlineData = readSource(outline_source);
+    outlineData = readSource(outline_source);
     outlineData = parseOutlineData(outlineData);
     var outlines = [];
     for (var borough in outlineData) {
